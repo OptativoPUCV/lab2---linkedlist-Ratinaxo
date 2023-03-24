0a6f51eb9a70a3409964c2a95ca0e7d24292caa0
Replit user <>
2023-03-24 01:30:01 +0000

    new try

--- list.c
+++ list.c
@@ -51,7 +51,8 @@ void * nextList(List * list) {
 }
 
 void * lastList(List * list) {
-  return NULL;
+  list->current = list->current->prev;
+  return list->current->data;
 }
 
 void * prevList(List * list) {
